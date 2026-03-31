a2b1f53be9eb6188633a54ee9e2463dd409b2265
diegorego516 <diegorego516@gmail.com>
2026-03-30 23:26:37 -0300

    Add files via upload

---
+++ SystemHack.py
@@ -0,0 +1,12 @@
+import scapy
+
+def hackear(os, sys):
+    if hackear in os.sys:
+        def sistema_alvo(os, sys, scapy):
+            if sistema_alvo in os.sys.scapy:
+                input("Insira aqui o nome do sistema alvo para hackear: ")
+                print("Sistema Hackeado.")
+        try:
+                print("Seu sistema foi hackeado.")
+        except Exception as e:
+                print("Você foi hackeado.")
