a60eeda723dd4f7ca3fa35dee0507204f607dcd2
diegorego516 <diegorego516@gmail.com>
2026-03-31 14:32:48 -0300

    Update SystemHack.py

--- SystemHack.py
+++ SystemHack.py
@@ -1,7 +1,35 @@
 import scapy
+import os
+import sys
 
-def hackear(os, sys):
-    if hackear in os.sys:
+self = scapy
+self = os
+self = sys
+
+system_login_bypass = True
+system_antívirus_bypass = True
+system_firewall_bypass = True
+
+def system_target_for_login_bypass(Windows11):
+
+    def antívirus_to_victimize_on_target_system(Windows11):
+        system_target_for_login_bypass = Windows11
+    antívirus_to_victimize_on_target_system = Windows11
+    def firewall_to_compromise_on_target_system(Windows11_Firewall):
+        firewall_to_compromise_on_target_system = Windows11_Firewall
+
+def sistema_alvo(Windows11):
+
+        sistema_alvo = Windows11
+
+def tacticas_de_intrusão(hackear_o_sistema):
+
+    tacticas_de_intrusão = hackear_o_sistema
+
+tentativas = 3
+
+def hackear_o_sistema(os, sys):
+    if hackear_o_sistema in os.sys:
         def sistema_alvo(os, sys, scapy):
             if sistema_alvo in os.sys.scapy:
                 input("Insira aqui o nome do sistema alvo para hackear: ")
